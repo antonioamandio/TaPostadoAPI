d8b1f533bb5d92e99e859785b36fe6fd7803bf12
antonioamandio <antonioamandio22@gmail.com>
2026-08-01 20:42:22 +0100

    feat(PostController): implement CRUD methods for post management

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -4,4 +4,40 @@ export class PostController {
     constructor(postService) {
         this.#postService = postService
     }
+
+    async create(req, res) {
+        const post = await this.#postService.create(req.body)
+
+        return res.status(201).json(post)
+    }
+
+    async findAll(req, res) {
+        const posts = await this.#postService.findAll()
+
+        return res.status(200).json(posts)
+    }
+
+    async findById(req, res) {
+        const { id } = req.params
+
+        const post = await this.#postService.findById(id)
+
+        return res.status(200).json(post)
+    }
+
+    async update(req, res) {
+        const { id } = req.params
+
+        const post = await this.#postService.update(id, req.body)
+
+        return res.status(200).json(post)
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+
+        const post = await this.#postService.delete(id)
+
+        return res.sendStatus(204)
+    }
 }
