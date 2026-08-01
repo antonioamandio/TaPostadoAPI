815c5306a7ca764d548e09460d672f899ca8487b
antonioamandio <antonioamandio22@gmail.com>
2026-08-01 21:03:59 +0100

    feat(CommentController): implement CRUD methods for comment management

--- src/controllers/CommentController.js
+++ src/controllers/CommentController.js
@@ -4,4 +4,40 @@ export class CommentController {
     constructor(commentService) {
         this.#commentService = commentService
     }
+
+    async create(req, res) {
+        const comment = await this.#commentService.create(req.body)
+
+        return res.status(201).json(comment)
+    }
+
+    async findAll(req, res) {
+        const comments = await this.#commentService.findAll()
+
+        return res.status(200).json(comments)
+    }
+
+    async findById(req, res) {
+        const { id } = req.params
+
+        const comment = await this.#commentService.findById(id)
+
+        return res.status(200).json(comment)
+    }
+
+    async update(req, res) {
+        const { id } = req.params
+
+        const comment = await this.#commentService.update(id, req.body)
+
+        return res.status(200).json(comment)
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+
+        await this.#commentService.delete(id)
+
+        return res.sendStatus(204)
+    }
 }
