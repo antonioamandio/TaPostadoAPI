30b2802118aa48647b0bb3d7721b004d81997231
antonioamandio <antonioamandio22@gmail.com>
2026-08-01 02:16:20 +0100

    feat(UserController): implement CRUD methods for user management

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,4 +4,49 @@ export class UserController {
     constructor(userService) {
         this.#userService = userService
     }
+
+    async create(req, res) {
+        const user = await this.#userService.create(req.body)
+
+        return res.status(201).json(user)
+    }
+
+    async findAll(req, res) {
+        const users = await this.#userService.findAll()
+
+        return res.status(200).json(users)
+    }
+
+    async findByEmail(req, res) {
+        const { email } = req.params
+
+        const user = await this.#userService.findByEmail(email)
+
+        return res.status(200).json(user)
+    }
+
+    async findById(req, res) {
+        const { id } = req.params
+
+        const user = await this.#userService.findById(id)
+
+        return res.status(200).json(user)
+    }
+
+    async update(req, res) {
+        const { id } = req.params
+        const { body } = req
+
+        const user = await this.#userService.update(id, body)
+
+        return res.status(200).json(user)
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+
+        await this.#userService.delete(id)
+
+        return res.status(204)
+    }
 }
